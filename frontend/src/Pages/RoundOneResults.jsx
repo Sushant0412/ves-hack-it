271ce9ac2bc1b4bb7e20a7f8ec8097f057601218
Sushant0412 <mikesushant05@gmail.com>
2026-03-18 20:18:12 +0530

    Updated shorlisted teams with team lead name

--- frontend/src/Pages/RoundOneResults.jsx
+++ frontend/src/Pages/RoundOneResults.jsx
@@ -4,66 +4,64 @@ import { useState } from "react"
 
 const softwareTeams = {
   "SW-01": [
-    "Byte Alchemy",
-    "Code Monk",
-    "AltX_OverDrive",
-    "Runtime Terror",
-    "SentriX",
-    "Green Tech",
-    "Hackonauts",
-    "CodeBreakers",
-    "InnovateX",
-    "Team_Technexis"
+    { team: "Byte Alchemy", lead: "Shubhada Deshmukh" },
+    { team: "Code Monk", lead: "Soham Patil" },
+    { team: "AltX_OverDrive", lead: "Nachiket Kale" },
+    { team: "Runtime Terror", lead: "Saurabh Gangurde" },
+    { team: "SentriX", lead: "Dhruti Mehta" },
+    { team: "Green Tech", lead: "Rohit Bhagwat" },
+    { team: "Hackonauts", lead: "Vedant Gawali" },
+    { team: "CodeBreakers", lead: "Jidnesh Patil" },
+    { team: "InnovateX", lead: "Asmi Katke" },
+    { team: "Team_Technexis", lead: "Arnav Shirwadkar" }
   ],
   "SW-02": [
-    "SayKnowMore",
-    "SurveyCorps",
-    "Code Titans",
-    "Code Watchers",
-    "DAWNIX",
-    "GDuo",
-    "JSHS",
-    "Gradient Descent",
-    "SYSCODE",
-    "TeenTigadas"
+    { team: "SayKnowMore", lead: "Chaitanya Ananda Medidar" },
+    { team: "SurveyCorps", lead: "Gaurav More" },
+    { team: "Code Titans", lead: "Suyash Patil" },
+    { team: "Code Watchers", lead: "Saumitra Gurav" },
+    { team: "DAWNIX", lead: "Suyash Sonawane" },
+    { team: "GDuo", lead: "Rushil Patil" },
+    { team: "JSHS", lead: "Hemant Jawale" },
+    { team: "Gradient Descent", lead: "Dhruv Shetty" },
+    { team: "SYSCODE", lead: "Sakshi Bhansali" },
+    { team: "TeenTigadas", lead: "Meet Phulwani" }
   ],
   "SW-03": [
-    "Chaat Masala",
-    "Scam Shield",
-    "Code & Chaos",
-    "Tandoori Techies",
-    "InnoVerse",
-    "Sentinel",
-    "Developers",
-    "ARKAA!",
-    "Hackoverflow",
-    "Akatsuki"
+    { team: "Chaat Masala", lead: "Amarnath" },
+    { team: "Scam Shield", lead: "Yogaant Naidu" },
+    { team: "Code & Chaos", lead: "Tanishka Donde" },
+    { team: "Tandoori Techies", lead: "Purjeet Shahu" },
+    { team: "InnoVerse", lead: "Vedant Shelar" },
+    { team: "Sentinel", lead: "Krish Pinto" },
+    { team: "Developers", lead: "Drishti Gupta" },
+    { team: "ARKAA!", lead: "Allan Fernandes" },
+    { team: "Hackoverflow", lead: "Soham Chaudhari" },
+    { team: "Akatsuki", lead: "Kushith Shetty" }
   ]
 }
 
+
 const hardwareTeams = {
-  "HW-01": [
-    "Innovation Insurgents"
-  ],
   "HW-02": [
-    "Team Rocket",
-    "MedByte",
-    "Runtime Error",
-    "IoTians",
-    "CGPAglus",
-    "ElectroBoom"
+    { team: "Team Rocket", lead: "Yash Jadhav" },
+    { team: "Runtime Error", lead: "Vansh Lalwani" },
+    { team: "IoTians", lead: "Sakib Khan" },
+    { team: "CGPAglus", lead: "Ishaan Vaidya" },
+    { team: "ElectroBoom", lead: "Kalpesh Kisan Naik" }
   ],
   "HW-03": [
-    "Team Vanguard",
-    "DeeDos",
-    "Hacker Logs",
-    "HACK ERROR 404"
+    { team: "Team Vanguard", lead: "Faizan Rabbani" },
+    { team: "DeeDos", lead: "Samarth Kale" },
+    { team: "Hacker Logs", lead: "Umed Indulkar" },
+    { team: "HACK ERROR 404", lead: "Aditya Basantani" },
+    { team: "BeRRY", lead: "Reyansh Bhavesh Sakariya" }
   ],
   "HW-04": [
-    "Xspark",
-    "Hackhive",
-    "karare kurkure",
-    "ShockSquad"
+    { team: "Xspark", lead: "Jui Adke" },
+    { team: "Hackhive", lead: "Apoorva Ghyare" },
+    { team: "karare kurkure", lead: "Raj Shukla" },
+    { team: "ShockSquad", lead: "Shubham Kandalgaonkar" }
   ]
 }
 
@@ -108,9 +106,10 @@ const RoundOneResults = () => {
           >
             <h2 className="text-3xl font-bold text-white mb-4 border-b border-white/20 pb-2">{ps}</h2>
             <ul className="space-y-2">
-              {teams.map((team, key) => (
-                <li key={key} className="text-white font-light hover:font-medium transition-all duration-200">
-                  {team}
+              {teams.map((item, key) => (
+                <li key={key} className="text-white transition-all duration-200 mb-3">
+                  <div className="text-xl font-semibold tracking-wide">{item.team}</div>
+                  <div className="text-sm text-gray-300">Lead: {item.lead}</div>
                 </li>
               ))}
             </ul>
